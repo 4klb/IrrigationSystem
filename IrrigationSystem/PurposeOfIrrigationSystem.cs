9daee24d649505861a9228c7da3dd44851e8f600
Hadz_xt61@student.itstep.kz <Hadz_xt61@student.itstep.kz>
2020-12-11 22:56:03 +0600

    Добавление класса PurposeOfIrrigationSystem и его полей

---
+++ IrrigationSystem/PurposeOfIrrigationSystem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IrrigationSystem
+{
+    public class PurposeOfIrrigationSystem
+    {
+        public string nameOfIrrigation;//название полива
+        public int volumeOfIrrigation;//объем полива
+        public string areaOfIrrigation;//территория орошения
+        public string timeOfIrrigation;//время орошения
+    }
+}
